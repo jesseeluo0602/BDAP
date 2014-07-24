c3dc9e671ad22db5eaaba163e071ea53cf9ec3b7
unknown <jesseeluo0602@gmail.com>
2014-07-24 14:31:23 -0700

    almost done

--- controllers.js
+++ controllers.js
@@ -1,10 +1,11 @@
   var app = angular.module('BDAP', ["ngAnimate"]);
 
-  app.controller('PMTController', function(){
+  app.controller('PMTController', function( ){
     this.pmt = pmtList;
     this.list = addedList;
-    this.add = function(){
-      this.list.push({pmt: '8512305'});
+    this.cool=0;
+    this.addItem = function(pmtnumber, tech){
+      this.list.push({pmt: pmtnumber, category: tech})
     }
   });
 
@@ -15,14 +16,14 @@
   app.filter('sort', function () {
     // function to invoke by Angular each time
     // Angular passes in the `items` which is our Array
-    return function (items, element) {
+    return function (items, tech) {
       // Create a new Array
       var filtered = [];
       // loop through existing Array
       for (var i = 0; i < items.length; i++) {
         var item = items[i];
         // check if the individual Array element begins with `a` or not
-        if (item.type === element || element==="All" ) {
+        if (item.Category === tech || tech==="All" ) {
           // push it into the Array if it does!
           filtered.push(item);
         }
@@ -35,14 +36,26 @@
 
 
 
-  var pmtList = [
-    { name: 'Holy Dragon', rank: 'A',  type:'Fire', imageUrl:"http://img2.wikia.nocookie.net/__cb20131222134656/quiz-rpg-the-world-of-mystic-wiz/images/6/61/Holy_Dragon_Icon.png", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/Holy_Dragon" },
-    { name: 'White Flame Holy Dragon', rank: 'A-',  type:"Fire", imageUrl:"http://img1.wikia.nocookie.net/__cb20131222134724/quiz-rpg-the-world-of-mystic-wiz/images/thumb/3/33/White_Flame_Holy_Dragon_Icon.png/100px-White_Flame_Holy_Dragon_Icon.webp", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/White_Flame_Holy_Dragon"},
-    { name: 'Fiery Dragon', rank: 'B', type:'Fire', imageUrl:"http://img4.wikia.nocookie.net/__cb20131222112237/quiz-rpg-the-world-of-mystic-wiz/images/thumb/5/5f/Fiery_Dragon_Icon.png/100px-Fiery_Dragon_Icon.png", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/Fiery_Dragon" },
-    { name: 'Riel', rank: 'A', type:'Water', imageUrl:"http://img2.wikia.nocookie.net/__cb20140106070142/quiz-rpg-the-world-of-mystic-wiz/images/thumb/9/9e/Riel_%28Aqua_Pistol%29_Icon.png/100px-Riel_%28Aqua_Pistol%29_Icon.webp", wiki:"http://quiz-rpg-the-world-of-mystic-wiz.wikia.com/wiki/Riel_(Aqua_Pistol)" },
-
+var pmtList = [
+{ PMT: 078945, Category: "Web Development",  IterationPts: "58", AvgTime: "15.00"},
+{ PMT: 395783, Category: "Mobile Applications Development",  IterationPts:"27", AvgTime: "08.30"},  
+{ PMT: 148764, Category: "Performance Testing",  IterationPts:"64", AvgTime: "24.86"},
+{ PMT: 034620, Category: "Web Design",  IterationPts:"58", AvgTime: "14.65"},
+{ PMT: 947638, Category: "Leadership Training",  IterationPts:"32", AvgTime: "07.54"},
+{ PMT: 349730, Category: "Web Development",  IterationPts:"85", AvgTime: "37.10"},  
+{ PMT: 455893, Category: "Web Development",  IterationPts:"46", AvgTime: "11.08"},
+{ PMT: 340847, Category: "Performance Testing",  IterationPts:"53", AvgTime: "12.97"},
+{ PMT: 349028, Category: "Mobile Applications Development",  IterationPts:"34", AvgTime: "10.12"},
+{ PMT: 087487, Category: "Mobile Applications Design",  IterationPts:"58", AvgTime: "16.73"},  
+{ PMT: 936044, Category: "U2L",  IterationPts:"54", AvgTime: "15.88"},
+{ PMT: 457266, Category: "Server Maintenance",  IterationPts:"76", AvgTime: "17.95"},
+{ PMT: 338432, Category: "Cloud Computing",  IterationPts:"72", AvgTime: "25.02"},
+{ PMT: 284009, Category: "Big Data Initiative",  IterationPts:"34", AvgTime: "19.45"},  
+{ PMT: 247678, Category: "Big Data Initiative",  IterationPts:"67", AvgTime: "28.86"},
+{ PMT: 346897, Category: "Mobile Applications Design",  IterationPts:"44", AvgTime: "45.23"},
   ];  
 
   var addedList = [
-    {pmt: '85105'}
+    {pmt: '85105', Category: "Web Development"},
+    {pmt: '23535', Category: "U2L"}
   ];
